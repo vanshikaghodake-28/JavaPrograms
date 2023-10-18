15b6a4794f1ece7b2b3c966535695fd5f93dc70a
vanshikaghodake-28 <105494665+vanshikaghodake-28@users.noreply.github.com>
2023-10-18 20:40:45 +0530

    add identifier file

---
+++ identifier/identifier.java
@@ -0,0 +1,14 @@
+/** Devlop a program which show use of identifier */
+
+class Example{
+	public static void main(String[] args){
+		int a = 10;
+		int A  = 20;
+		int abcd = 70;
+		int a_apple = 40;
+		//char 2a = 23; 
+		//int 2 =10;
+		char & = 34;
+		//int _ = 7;
+	}
+}
